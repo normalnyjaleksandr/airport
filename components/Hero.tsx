ab4c06ccc6eb9df25d48f4d5eea640c6910bf112
normalnyjaleksandr <normalnyjaleksandr.2@gmail.com>
2026-06-09 04:18:13 +0300

    Dev container (#3)
    * Create devcontainer.json
    
    * скелет next js
    
    * Package.
    
    * Fix

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+  return (
+    <section className="hero-card">
+      <h1>Airport Next.js Skeleton</h1>
+      <p>Welcome to your new Next.js app router project.</p>
+      <ul>
+        <li>TypeScript ready</li>
+        <li>App directory structure</li>
+        <li>Global styling setup</li>
+      </ul>
+    </section>
+  );
+}
